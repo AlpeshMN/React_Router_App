4324d857fb4642a16edf103e87a97d929354fcd7
Nakrani, Alpesh <alpesh.nakrani@anthem.com>
2019-05-05 19:29:45 -0400

    Fixed routing in app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, NavLink, Redirect, Prompt} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+
+const User = (params) => {
+  return (
+    <h1>Welcome {params.username}</h1>
+  )
+}
 
 class App extends Component {
-  render() {
+  state = {
+    isLoggedIn: false
+  };
+
+  logInHandle = () => {
+    this.setState(prevState => ({
+      isLoggedIn: !prevState.isLoggedIn
+    }));
+  }
+
+   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <NavLink to="/" exact activeStyle={{ color: "green" }}>
+            Home
+          </NavLink>
+          <NavLink to="/about" exact activeStyle={{ color: "green" }}>
+            About
+          </NavLink>
+          <NavLink to="/user/alpesh" exact activeStyle={{ color: "green" }}>
+            Alpesh
+          </NavLink>
+          <NavLink to="/user/patel" exact activeStyle={{ color: "green" }}>
+            Patel
+          </NavLink>
+          <br />
+          <Prompt
+            when={!this.state.isLoggedIn}
+            message={(location)=>{
+              return location.pathname.startsWith('/user') ? 'Are you sure want to navigate?' : true;
+            }}
+          />
+          <input
+            type="button"
+            value={this.state.isLoggedIn ? "Log Out" : "Log In"}
+            onClick={this.logInHandle}
+          />
+          <Route
+            path="/"
+            exact
+            strict
+            render={() => {
+              return <h1>Welcome Home...</h1>;
+            }}
+          />
+          <Route
+            path="/about"
+            exact
+            strict
+            render={() => {
+              return <h1>Welcome About...</h1>;
+            }}
+          />
+          {/* <Route path="/user/:username" exact component={User} /> */}
+          <Route path="/user/:username" exact render={({match})=>{
+            return this.state.isLoggedIn ? (<User username={match.params.username} />) : (<Redirect to="/" />)
+          }} />
+        </div>
+      </Router>
     );
   }
 }
